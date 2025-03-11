21f6b89dd6ffd03b580fc5f74ec64b89a621e3f6
jesureyes <161269641+jesuryes@users.noreply.github.com>
2025-03-11 16:05:32 -0600

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,17 @@
+import StudentTable from "../components/StudentTable";
+
+
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-4xl text-center mt-8">
+        ¿Crees poder lograrlo? Si no confias en ti, quien lo hará?
+      </h1>
+      <h2 className="text-2xl text-center mt-4">
+        Aquí debe de verse tu tabla, ¿cuál tabla? Lee el archivo README 
+      </h2>
+      <StudentTable/>
+    </>
+  );
+}
